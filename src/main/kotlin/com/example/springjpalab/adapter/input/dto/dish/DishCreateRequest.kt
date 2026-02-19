bafebf8c5c9c3eac152b118cc8ec9cbb1f42e120
lidiya <karavaeva.lidaa@yandex.ru>
2026-02-19 18:39:09 +0700

    fix DishCreateRequest.kt

--- src/main/kotlin/com/example/springjpalab/adapter/input/dto/dish/DishCreateRequest.kt
+++ src/main/kotlin/com/example/springjpalab/adapter/input/dto/dish/DishCreateRequest.kt
@@ -16,7 +16,6 @@ data class DishCreateRequest(
 
     @field:NotNull(message = "Price cannot be null")
     @field:DecimalMin(value = "0.01", message = "Price must be greater than 0")
-    @field:DecimalMax(value = "499.0", message = "Price must be less than 499")
     val price: Number,
 
     @field:NotNull(message = "Availability must be specified")
