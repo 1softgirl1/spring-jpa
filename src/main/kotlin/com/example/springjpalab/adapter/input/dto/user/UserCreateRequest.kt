19534a4946aa5701c34f2393d8486d65a7e0f44a
lidiya <karavaeva.lidaa@yandex.ru>
2026-03-08 20:35:15 +0700

    fix field active -> isActive

--- src/main/kotlin/com/example/springjpalab/adapter/input/dto/user/UserCreateRequest.kt
+++ src/main/kotlin/com/example/springjpalab/adapter/input/dto/user/UserCreateRequest.kt
@@ -1,5 +1,6 @@
 package com.example.springjpalab.adapter.input.dto.user
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
@@ -17,5 +18,7 @@ data class UserCreateRequest(
     @field:Size(min = 1, message = "LastName must be at least 1 character")
     val lastName: String,
 
+    @JsonProperty("isActive")
+    @field:NotBlank(message = "isActive cannot be blank")
     val isActive: Boolean = true,
 )
--- src/main/kotlin/com/example/springjpalab/adapter/input/dto/user/UserUpdateRequest.kt
+++ src/main/kotlin/com/example/springjpalab/adapter/input/dto/user/UserUpdateRequest.kt
@@ -1,5 +1,6 @@
 package com.example.springjpalab.adapter.input.dto.user
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
@@ -19,5 +20,6 @@ data class UserUpdateRequest(
     val lastName: String,
 
     @field:NotNull(message = "Active cannot be null or empty")
+    @JsonProperty("isActive")
     val isActive: Boolean
 )
